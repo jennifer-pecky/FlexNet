f7fb1a59a43fb841f9086238020495b28230f25c
jennifer-pecky <ugwojennifer7@gmail.com>
2023-03-18 21:30:48 +0100

    workedon navbr

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -6,3 +6,11 @@
 // button.addEventListener(click, () => {
 //     button.classList.toggle('active')
 // })
+
+
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
