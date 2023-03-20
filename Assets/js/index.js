904008b727c6ea93250e9673690de601661c8105
jennifer-pecky <ugwojennifer7@gmail.com>
2023-03-20 23:41:13 +0100

    worked on handburger menu

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -3,14 +3,26 @@
 
 // const button = document.querySelector('button')
 
+
 // button.addEventListener(click, () => {
 //     button.classList.toggle('active')
 // })
 
 
-const btn = document.querySelector("button.mobile-menu-button");
-const menu = document.querySelector(".mobile-menu");
+// const btn = document.querySelector("button.mobile-menu-button");
+// const menu = document.querySelector(".mobile-menu");
 
-btn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-});
+// btn.addEventListener("click", () => {
+//     menu.classList.toggle("hidden");
+// });
+
+
+
+
+let x = document.querySelector('.menu-bar');
+let menu = document.querySelector('.dropdown-content');
+
+x.onclick = function () {
+    this.classList.toggle("change");
+    menu.classList.toggle("menu_change");
+}
